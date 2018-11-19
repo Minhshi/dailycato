3868a7f8173afd1b366647d613d7c0cf26558d56
Minh Pham <mpham@naver.com>
2018-11-19 21:21:21 +0100

    Add db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_11_19_155850) do
-=======
-ActiveRecord::Schema.define(version: 2018_11_19_152541) do
-
->>>>>>> 589d6a479004bbb5e061b5511bb8e250e71f07a7
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +22,6 @@ ActiveRecord::Schema.define(version: 2018_11_19_152541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -44,8 +38,6 @@ ActiveRecord::Schema.define(version: 2018_11_19_152541) do
     t.datetime "updated_at", null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
-
-  add_foreign_key "reviews", "restaurants"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,9 +56,5 @@ ActiveRecord::Schema.define(version: 2018_11_19_152541) do
     t.index ["restaurant_id"], name: "index_users_on_restaurant_id"
   end
 
-<<<<<<< HEAD
-=======
-  add_foreign_key "users", "restaurants"
-
->>>>>>> 589d6a479004bbb5e061b5511bb8e250e71f07a7
+  add_foreign_key "reviews", "restaurants"
 end
