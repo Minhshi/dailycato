fc3df867b5d1316af5d0936f6e6dc15d8fd062f5
Eike Feld <eike.feld@gmx.de>
2018-11-20 15:24:17 +0100

    DB rework

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+timraue = User.create!(email: "tim.raue@web.de", first_name: "Tim", last_name: "Raue", password: "123123")
+Restaurant.delete_all
+restaurants = Restaurant.create!(name: 'Tim Raue', address: "Rudi-Dutschke-Straße 7",  cuisine: "deutsche küche", owner: timraue)
