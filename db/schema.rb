4071fd32b4d905d8e509698031816af3121d81c2
Eike Feld <43243936+eikcmd@users.noreply.github.com>
2018-11-21 14:54:36 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2018_11_21_101908) do
-
 ActiveRecord::Schema.define(version: 2018_11_21_102707) do
 
 
