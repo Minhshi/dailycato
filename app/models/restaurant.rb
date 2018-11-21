8f2943ab37f834e18fc145c86bf43fbe99281c08
Eike Feld <eike.feld@gmx.de>
2018-11-21 14:43:39 +0100

    rework scheema

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,14 @@
 class Restaurant < ApplicationRecord
   has_many :deals
   has_many :reviews
-  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+  belongs_to :user
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :cuisine, inclusion: ["amerikanisch", "französisch", "asiatisch", "burger", "dessert", "deutsche küche", "fisch", "gesund", "exklusive deals", "indisch", "italienisch", "japanisch", "koreanisch", "mediterranean", "mexikanisch", "pizza", "sandwiches", "sushi", "syrisch", "thailändisch", "türkisch", "vegetarisch", "vietnamesisch"]
+  validates :cuisine, inclusion: ["amerikanisch", "französisch", "asiatisch", "burger", "dessert", "deutsche küche", "fisch", "gesund", "exklusive deals", "indisch", "italienisch", "japanisch", "koreanisch", "mediterranean", "mexikanisch", "pizza", "sandwiches", "sushi", "syrisch", "thailändisch", "türkisch", "vegetarisch", "vietnamesisch", "other"]
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :owner, presence: true
+  validates :user, presence: true
 end
