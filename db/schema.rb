487ae70a87e0a57e8322f56f7ed4eb5aa3b5c3cd
Eike Feld <43243936+eikcmd@users.noreply.github.com>
2018-11-21 14:56:20 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_102707) do
     t.bigint "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["restaurant_id"], name: "index_users_on_restaurant_id"
